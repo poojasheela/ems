1c15c02cc72d7bc8012bdcf3e875a64ffea835bf
poojasheela <pooja_sheela@mystiquex.com>
2025-09-15 16:23:20 +0530

    add

--- src/test/java/com/example/ems/EmsApplicationTests.java
+++ src/test/java/com/example/ems/EmsApplicationTests.java
@@ -1,8 +1,10 @@
 package com.example.ems;
+import com.example.ems.config.GithubProperties;
 import com.example.ems.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.vault.config.VaultAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,6 +22,9 @@ import org.springframework.test.context.ActiveProfiles;
 @EnableAutoConfiguration(exclude = { VaultAutoConfiguration.class })
 @ActiveProfiles("test")
 class EmsApplicationTests {
+	@MockBean
+	private GithubProperties githubProperties;
+
 	@Test
 	void contextLoads() {}
 }
